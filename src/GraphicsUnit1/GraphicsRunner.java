3e9a7dbbb6ffa81040c276db677bdbc542be0599
finkelmanj7070 <finkelmanj7070@CA-SU-F106-25.sduhsd.lan>
2019-02-27 10:19:31 -0800

    First Commit

---
+++ src/GraphicsUnit1/GraphicsRunner.java
@@ -0,0 +1,40 @@
+package GraphicsUnit1;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import javax.swing.JFrame;
+
+public class GraphicsRunner extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600; //final is constant to make graphics constant
+
+	public GraphicsRunner()
+	{
+		super("Graphics Runner"); //sets title
+
+		setSize(WIDTH,HEIGHT); //jframe type of method
+
+		getContentPane().add(new SmileyFace());
+		
+		//add other classes to run them 
+		//BigHouse, Robot, or ShapePanel 
+                
+             //   getContentPane().add(new BigHouse());
+
+		setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //stops program when window closed
+	}
+
+	public static void main( String args[] )
+	{
+		GraphicsRunner run = new GraphicsRunner();
+	}
+}
