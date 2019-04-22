32e6fb4133ef0965e1e648692c5c2c1024ccd3d0
finkelmanj7070 <finkelmanj7070@CA-SU-F106-25.sduhsd.lan>
2019-04-22 09:29:06 -0700

    April 22 School

---
+++ src/WinterScene/FancySnowFlake.java
@@ -0,0 +1,51 @@
+package WinterScene;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+//extend the AbstractShape class to make a FancySnowFlake class
+public class FancySnowFlake extends AbstractShape {
+    public FancySnowFlake(int x, int y, int wid, int ht)
+   {
+       super(x,y,wid,ht);
+   }
+   
+   public FancySnowFlake(int x, int y, int wid, int ht, Color col)
+   {
+		super(x,y,wid,ht,col);
+   }   
+   
+   public FancySnowFlake(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
+   {
+		super(x,y,wid,ht,col,xSpd,ySpd);
+   }  
+   
+   public void draw(Graphics window) {
+      // window.setColor(Color.WHITE);
+       
+   }
+   
+   public void moveAndDraw(Graphics window) {
+       window.setColor(Color.BLUE);
+       draw(window);
+       
+      setXPos(getXPos()+getXSpeed());
+		//setY
+      setYPos(getYPos() + getYSpeed());
+      
+      window.setColor(Color.WHITE);
+      draw(window);
+       
+       
+   }
+   
+   public String toString() {
+       return super.getXPos()+" "+super.getYPos()+" "+super.getWidth()+" "+super.getHeight()+" "+super.getColor()+" "+super.getXSpeed()+" "+super.getYSpeed();
+   }
+}
+
